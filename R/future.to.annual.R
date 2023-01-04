64deca406cd47b730b96c952be8ede0c373f5e42
Mckay <1248278575121002@mil>
2023-01-04 16:13:37 -0500

    Kyle edited future.to.annual with the crew

--- R/future.to.annual.R
+++ R/future.to.annual.R
@@ -1,27 +1,30 @@
-#' Compute annual value from accumulated amount after years
-#' using sinking fund factor (sff) 
+#' Compute annual payment value from future value 
 #'  
-#' \code{future.to.annual} calculate annual value from future value 
+#' \code{future.to.annual} calculate annual value from future value using
+#'      the accumulated amount after years using sinking fund factor (sff) 
 #'     
-#'Function for computing annual value based on:
-#'@input discount rate (dr)
-#'@input life span in years (yr)
-#'@input accumulated(future)amount (fr.value)
+#' @param dr discount rate
+#' @param span life span in years
+#' @param fr.value accumulated (future) amount 
 #'
 #' @return ann.value 
 #'
 #' @references
-#'
+#' Newnan, Donald G., Ted G. Eschenbach, and Jerome P. Lavelle. 
+#'      Engineering economic analysis. Vol. 12. Oxford University Press, 2012
 #'  
 #' @examples  
 #' # Result: ann.value 
-#' ann.value(0.08, 650000, 30)  
+#' future.to.annual(0.08, 30, 650000)  
 #' 
-
-ann.value <- function(dr, fr.value, yr){
+#' @export
+future.to.annual <- function(dr, span, fr.value){
   # compute sinking fund factor
-  sff <- dr/((1+dr)^yr-1)
+  sff <- dr / ((1+dr)^span-1)
   
   # compute annual value of costs
-  ann.value <-(fr.value)(caf)
+  ann.value <- fr.value * sff
+  
+  #Send the output
+  ann.value
 }
