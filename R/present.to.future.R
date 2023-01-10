57aee8b3597776caaf5ac6857f86c2985b378f6b
liyabera <liya.eshetu.abera@gmail.com>
2023-01-10 15:33:46 -0600

    updated

--- R/present.to.future.R
+++ R/present.to.future.R
@@ -1,29 +1,30 @@
 #' Compute future values from present value
-#' using compound amount factor (caf) 
-#'  
-#' \code{present.to.future} calculate future value from present value  
+#' 
+#' \code{present.to.future} calculate future value from present value using
+#'      compound amount factor (caf)   
 #'     
-#'Function for computing future value based on:
-#'@input discount rate (dr)
-#'@input life span in years (yr)
-#'@input present value (pv)
+#' @param dr discount rate in percent per year
+#' @param span life span in years 
+#' @param pv.value present value
 #'
-#' @return fr.from.pv
+#' @return fr.value
 #'
-#'#'@references
+#' @references
 #'Newnan, Donald G., Ted G. Eschenbach, and Jerome P. Lavelle. 
-#'Engineering economic analysis. Vol. 12. Oxford University Press, 2012
+#'      Engineering economic analysis. Vol. 12. Oxford University Press, 2012
 #'  
 #' @examples  
-#' # Result: fr.from.pr 
-#' fr.from.pr(0.08, 900000, 30)  
+#' # Result: fr.value 
+#' present.to.future (0.08, 30, 900000)  
 #' 
-
-fr.from.pr <- function(dr, pv, yr){
+#' @export
+present.to.future <- function(dr, span, pv.value){
   # compute compound amount factor 
   caf <-(1+dr)^yr
   
   # compute future value of costs
-  fr.from.pr <-(pv)(caf)
+  fr.value <- pv.value * caf
   
+  #Send the output
+  fr.value
 }
