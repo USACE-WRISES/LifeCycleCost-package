f84aee3e85714ff450e41ff098e32093f3b2f26e
liyabera <liya.eshetu.abera@gmail.com>
2023-01-10 11:40:45 -0600

    Edited

--- R/annual.to.future.R
+++ R/annual.to.future.R
@@ -5,25 +5,25 @@
 #'     
 #' @param dr discount rate in percent per year
 #' @param span life span in years
-#' @param ann series of uniform annual payments
+#' @param ann.value series of uniform annual payments
 #'
-#' @return Cost in future value 
+#' @return fr.value 
 #'
 #' @references
 #' Newnan, Donald G., Ted G. Eschenbach, and Jerome P. Lavelle. 
 #'      Engineering economic analysis. Vol. 12. Oxford University Press, 2012
 #'  
 #' @examples  
-#' # Result: Future Value 
+#' # Result: fr.value 
 #' annual.to.future(0.08, 30, 50000)  
 #' 
 #' @export
-annual.to.future <- function(dr, span, ann){
+annual.to.future <- function(dr, span, ann.value){
   # compute uniform series compound amount factor
   uscaf <- ((1+dr)^span - 1) / dr
   
   # compute future value of costs
-  fr.value <- ann * uscaf
+  fr.value <- ann.value * uscaf
   
   #Compute total future value
   sum(fr.value)
