68db43c39a59c51dc6f8bf06b86f75b70a656c73
liyabera <liya.eshetu.abera@gmail.com>
2023-01-18 09:02:30 -0600

    Variables Changed

--- R/annual.to.present.R
+++ R/annual.to.present.R
@@ -3,32 +3,32 @@
 #' \code{annual.to.present} Compute present value from uniform annual payments 
 #'      using uniform series present worth factor (uswf)
 #'      
-#' @param dr discount rate in percent per year
-#' @param span life span in years
-#' @param ann.value series of uniform annual payments
+#' @param i discount rate in percent per year
+#' @param n life span in years
+#' @param A series of uniform annual payments
 #'
-#' @return pr.value
+#' @return P
 #'
 #' @references
 #' Newnan, Donald G., Ted G. Eschenbach, and Jerome P. Lavelle. 
 #'      Engineering economic analysis. Vol. 12. Oxford University Press, 2012.
 #'  
 #' @examples  
-#' # Result: pr.value = $33,3773 
+#' # Result: P = $33,3773 
 #' annual.to.present(0.08, 30, 30000)  
 #' 
 #' @export
-annual.to.present <- function(dr, span, ann.value){
+annual.to.present <- function(i, n, A){
   # compute uniform series present worth factor
-  uswf <-((1+dr)^span-1) / (dr*(1+dr)^span)
+  uswf <-((1+i)^n-1) / (i*(1+i)^n)
   
   # compute present value of costs
-  Pr.value <- ann.value * uswf
+  P <- A * uswf
 
   #Compute total present value
-   sum(Pr.value.ann)
+   sum(P)
    
   #Send the output
-  ann.value
+  A
   
 }
