22fdd6e2cac932d9e60b7762409da5925f32e1da
liyabera <liya.eshetu.abera@gmail.com>
2023-01-18 09:28:53 -0600

    Variables changed

--- R/future.to.annual.R
+++ R/future.to.annual.R
@@ -3,28 +3,28 @@
 #' \code{future.to.annual} calculate annual value from future value using
 #'      the accumulated amount after years using sinking fund factor (sff) 
 #'     
-#' @param dr discount rate in percent per year
-#' @param span life span in years
-#' @param fr.value accumulated (future) amount 
+#' @param i discount rate in percent per year
+#' @param n life span in years
+#' @param FV accumulated (future) value 
 #'
-#' @return ann.value 
+#' @return A 
 #'
 #' @references
 #' Newnan, Donald G., Ted G. Eschenbach, and Jerome P. Lavelle. 
 #'      Engineering economic analysis. Vol. 12. Oxford University Press, 2012
 #'  
 #' @examples  
-#' # Result: ann.value 
+#' # Result: A
 #' future.to.annual(0.08, 30, 650000)  
 #' 
 #' @export
-future.to.annual <- function(dr, span, fr.value){
+future.to.annual <- function(i, n, FV){
   # compute sinking fund factor
-  sff <- dr / ((1+dr)^span-1)
+  sff <- i / ((1+i)^n-1)
   
   # compute annual value of costs
-  ann.value <- fr.value * sff
+  A <- FV * sff
   
   #Send the output
-  ann.value
+  A
 }
